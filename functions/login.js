b3edbb8989966e82ef91ebe5eb968806f556f8c1
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-04-13 09:35:30 -0700

    remove comments, add global styles

--- functions/login.js
+++ functions/login.js
@@ -1,19 +1,3 @@
-// const handlers = {
-//   POST: async (req, res) => {
-//     /* Step 4.2: Validate the user's DID token */
-
-//     /* Step 4.3: Get or create a user's entity in FaunaDB */
-
-// Once we have the user's verified information, we can create
-// a session cookie! As this is not the primary topic of our tutorial
-// today, we encourage you to explore the implementation of
-// `createSession` on-your-own to learn more!
-// await createSession(res, { ... })
-
-//     res.status(200).send({ done: true })
-//   },
-// }
-
 // const { UserModel } = require( './lib/models/user-model')
 const { magic } = require("./lib/magic");
 const { createSession } = require("./lib/auth-cookies");
@@ -33,6 +17,7 @@ exports.handler = async (event, res) => {
     (await userModel.createUser(email));
   const token = await userModel.obtainFaunaDBToken(user);
 
+  // TODO: Refactor createSession into Netlify return value. Check what it wants.
   await createSession(res, { token, email, issuer });
 
   return {
